c75d14ea8b3751f6ea52bd3f87c312cf8362b796
sneha shinde <Sneha06032000@gmail.com>
2022-07-01 18:36:13 +0530

    1st que

--- index.js
+++ index.js
@@ -38,17 +38,47 @@
 // 55 89 144 233 377
 
 
-let a=0;
-let b=1;
-for(row=a;row<=5;row++){
-  let stars = '';
-  for(col=1;col<=row;col++){
-     stars +=' ';
-    let c=a+b;
-    stars+=`${a}`;
-    a=b;
-    b=c;
+// let a=0;
+// let b=1;
+// for(row=a;row<=5;row++){
+//   let stars = '';
+//   for(col=1;col<=row;col++){
+//      stars +=' ';
+//     let c=a+b;
+//     stars+=`${a}`;
+//     a=b;
+//     b=c;
     
+//   }
+//   console.log(stars);
+// }
+
+// Que no 1
+//Input
+// You will be given a number n
+// 1 <= n <= 100
+// Output
+// Print the following pattern
+// Sample Output:
+// For n = 5
+// *              *
+//    *       *
+//        *
+//    *       *
+// *              *
+
+let n=5;
+for(let row=1;row<=5;row++){
+  let stars='';
+  for(let col=1;col<=5;col++){
+   if(row==col || (row+col==6)){
+    stars += '  *  ';
+   }
+   else{
+    stars += '     ';
+   }
   }
   console.log(stars);
+  
 }
+
