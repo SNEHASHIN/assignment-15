a028e2a3d67dc12cb5532b10fa9d02265bf28590
sneha shinde <Sneha06032000@gmail.com>
2022-07-01 18:24:10 +0530

    4th que

--- index.js
+++ index.js
@@ -12,16 +12,43 @@
 // 7 8 9 10
 // 11 12 13 14 15
 
-let n=5, count = 1;
-let star = "";
-for(let row=0;row<n;row++){
-  for(let col=0;col<=row;col++){
-    star  += count;
-    star += " ";
-    count++ ;
-  }
-  star += "\n";
-}
-console.log(star);
+// let n=5, count = 1;
+// let star = "";
+// for(let row=0;row<n;row++){
+//   for(let col=0;col<=row;col++){
+//     star  += count;
+//     star += " ";
+//     count++ ;
+//   }
+//   star += "\n";
+// }
+// console.log(star);
 
- 
+// Input
+// You will be given a number n
+// 1 <= n <= 100
+// Output
+// Print the following pattern
+// Sample Output:
+// For n = 5
+// 0
+// 1 1
+// 2 3 5
+// 8 13 21 34
+// 55 89 144 233 377
+
+
+let a=0;
+let b=1;
+for(row=a;row<=5;row++){
+  let stars = '';
+  for(col=1;col<=row;col++){
+     stars +=' ';
+    let c=a+b;
+    stars+=`${a}`;
+    a=b;
+    b=c;
+    
+  }
+  console.log(stars);
+}
